67aec6194203b1ffdc203688606c6aa4828d6cce
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-02-09 00:25:55 +0300

    update tamamlandı.

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
-using TS.Result;
 using WebApi.Dtos;
 using WebApi.Options;
 using WebApi.Services;
@@ -8,6 +7,7 @@ using WebApi.Services;
 namespace WebApi.Controllers;
 [Route("api/[controller]/[action]")]
 [ApiController]
+//[Authorize]
 public sealed class UsersController(KeycloakService keycloakService, IOptions<KeycloakConfiguration> options) : ControllerBase
 {
     [HttpGet]
@@ -16,5 +16,43 @@ public sealed class UsersController(KeycloakService keycloakService, IOptions<Ke
         string endpoint = $"{options.Value.HostName}/admin/realms/{options.Value.Realm}/users";
         var response = await keycloakService.GetAsync<List<UserDto>>(endpoint, true, cancellationToken);
         return StatusCode(Response.StatusCode, response);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetByEmail(string email ,CancellationToken cancellationToken)
+    {
+        string endpoint = $"{options.Value.HostName}/admin/realms/{options.Value.Realm}/users?email={email}";
+        var response = await keycloakService.GetAsync<List<UserDto>>(endpoint, true, cancellationToken);
+        return StatusCode(Response.StatusCode, response);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetByUserName(string username, CancellationToken cancellationToken)
+    {
+        string endpoint = $"{options.Value.HostName}/admin/realms/{options.Value.Realm}/users?username={username}";
+        var response = await keycloakService.GetAsync<List<UserDto>>(endpoint, true, cancellationToken);
+        return StatusCode(Response.StatusCode, response);
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetById(Guid id, CancellationToken cancellationToken)
+    {
+        string endpoint = $"{options.Value.HostName}/admin/realms/{options.Value.Realm}/users/{id}";
+        var response = await keycloakService.GetAsync<UserDto>(endpoint, true, cancellationToken);
+        return StatusCode(Response.StatusCode, response);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update(Guid id,UpdateUserDto request, CancellationToken cancellationToken = default)
+    {
+        string endpoint = $"{options.Value.HostName}/admin/realms/{options.Value.Realm}/users/{id}";
+
+        var response = await keycloakService.PutAsync<string>(endpoint, request, true, cancellationToken);
+
+        if (response.IsSuccessful && response.Data is null)
+        {
+            response.Data = "UserUpdate is successfull";
+        }
+        return StatusCode(response.StatusCode, response);
+
     }
 }
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,10 +1,39 @@
-using Scalar.AspNetCore;
+using Keycloak.AuthServices.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 using WebApi.Options;
 using WebApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors();
+
+builder.Services.AddSwaggerGen(setup =>
+{
+    var jwtSecuritySheme = new OpenApiSecurityScheme
+    {
+        BearerFormat = "JWT",
+        Name = "JWT Authentication",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+        Description = "Put **_ONLY_** yourt JWT Bearer token on textbox below!",
+
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+
+    setup.AddSecurityDefinition(jwtSecuritySheme.Reference.Id, jwtSecuritySheme);
+
+    setup.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    { jwtSecuritySheme, Array.Empty<string>() }
+                });
+});
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
 
 
@@ -12,6 +41,9 @@ builder.Services.Configure<KeycloakConfiguration>(builder.Configuration.GetSecti
 builder.Services.AddScoped<KeycloakService>();
 
 builder.Services.AddControllers();
+
+builder.Services.AddKeycloakWebApiAuthentication(builder.Configuration);
+builder.Services.AddAuthentication();
 
 var app = builder.Build();
 app.UseHttpsRedirection();
@@ -22,10 +54,11 @@ app.UseCors(x =>
     x.AllowAnyMethod();
     x.AllowAnyOrigin();
 });
-app.MapOpenApi();
-app.MapScalarApiReference();
+app.UseSwagger();
+app.UseSwaggerUI();
 
-
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapGet("/get-access-token", async (KeycloakService keycloakService) =>
 {
