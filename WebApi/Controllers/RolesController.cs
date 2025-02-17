07a57c4a7749b20a1baba4764d3c993a8abe2c5a
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-02-17 10:44:09 +0300

    create role, get role by name , delete role by name

--- WebApi/Controllers/RolesController.cs
+++ WebApi/Controllers/RolesController.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using WebApi.Dtos;
 using WebApi.Options;
@@ -21,4 +19,42 @@ public class RolesController(
         var response = await keycloakService.GetAsync<List<RoleDto>>(endpoint, true, cancellationToken);
         return StatusCode(Response.StatusCode, response);
     }
+
+    [HttpGet]
+    public async Task<IActionResult> GetByName(string name, CancellationToken cancellationToken)
+    {
+        string endpoint = $"{options.Value.HostName}/admin/realms/{options.Value.Realm}/clients/{options.Value.ClientUUID}/roles/{name}";
+        var response = await keycloakService.GetAsync<RoleDto>(endpoint, true, cancellationToken);
+        return StatusCode(Response.StatusCode, response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateForClient(CreateRoleDto createRoleDto, CancellationToken cancellationToken)
+    {
+        string endpoint = $"{options.Value.HostName}/admin/realms/{options.Value.Realm}/clients/{options.Value.ClientUUID}/roles";
+        var response = await keycloakService.PostAsync<string>(endpoint, createRoleDto, true, cancellationToken);
+
+        if (response.IsSuccessful && response.Data is null)
+        {
+            response.Data = "Role create is successful";
+        }
+        return StatusCode(Response.StatusCode, response);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteByName(string name, CancellationToken cancellationToken)
+    {
+        string endpoint = $"{options.Value.HostName}/admin/realms/{options.Value.Realm}/clients/{options.Value.ClientUUID}/roles/{name}";
+        var response = await keycloakService.DeleteAsync<string>(endpoint, true, cancellationToken);
+
+        if (response.IsSuccessful && response.Data is null)
+        {
+            response.Data = "Role delete is successful";
+        }
+        return StatusCode(Response.StatusCode, response);
+
+        
+    }
+
+
 }
---
+++ WebApi/Dtos/CreateRoleDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApi.Dtos;
+
+public sealed class CreateRoleDto
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = default!;
+    [JsonPropertyName("description")]
+    public string Description { get; set; } = default!;
+}
+    
+
